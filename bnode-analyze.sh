6704dd7a4857c18a9ae6c495dd2f324df91abf14
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-05-10 14:49:12 +0200

    wip

--- bnode-analyze.sh
+++ bnode-analyze.sh
@@ -29,22 +29,68 @@ export REMOTE="$1"
 # Try to determine missing values automatically
 JSON=`sparql-integrate check-version-*.sparql <(echo 'SELECT ?s { ?s a sd:ServiceDescription }') --jq`
 
-if [ -z "$DBMS_VERSION" ]; then DBMS_VERSION=`echo "$JSON" | jq -r ".[].s.dbmsVersion[]"`; fi
 if [ -z "$DBMS_ID" ]; then DBMS_ID=`echo "$JSON" | jq -r ".[].s.dbmsId[]"`; fi
+if [ -z "$DBMS_VERSION" ]; then DBMS_VERSION=`echo "$JSON" | jq -r ".[].s.dbmsVersion[]"`; fi
 
 
+# Print settings to enable sanity check
+echoerr "-------------------------------------------------------"
 echoerr -e "DBMS Id: $DBMS_ID\nDBMS Version: $DBMS_VERSION\nURL: $REMOTE\n"
 
 
 # Request missing mandatory arguments
-if [ -z "$DBMS_VERSION" ]; then echoerr "[ERROR] dbms version is missing."; fi
-if [ -z "$DBMS_ID" ]; then echoerr "[ERROR] dbms name is missing."; fi
-if [ -z "$REMOTE" ]; then echoerr "[ERROR] Remote endpoint url is missing"; fi
+error=0
+if [ -z "$DBMS_ID" ]; then error=1; echoerr "[ERROR] dbms name is missing."; fi
+if [ -z "$DBMS_VERSION" ]; then error=1; echoerr "[ERROR] dbms version is missing."; fi
+if [ -z "$REMOTE" ]; then error=1; echoerr "[ERROR] Remote endpoint url is missing"; fi
+
+[ $error -eq 0 ] || exit $error
 
 
+#export REMOTE
+export DBMS_ID
+export DBMS_VERSION
 
-#REMOTE=http://localhost:8890/sparql sparql-integrate model.sparql spo.sparql
+
+#REMOTE=http://localhost:8890/sparql
+sparql-integrate model.sparql spo.sparql
 #REMOTE="$1" STORE_NAME="$2" STORE_VERSION="$3" sparql-integrate model.sparql spo.sparql
+
+
+# Obtain a bnode report - that is an rdf document which for a specific bnode holds information
+# about the results of different approaches for obtaining the internal blank node identifier
+BNODE_REPORT=`sparql-integrate select-bnode2.sparql <(echo 'SELECT ?s { ?s a eg:BnodeReport }') --jq`
+
+# Sanity check to detect empty rdf graphs: The report must refer to a blank node
+# TODO impl
+
+
+BNODE_ID_HPAFN=`jq -r '.[].s.hpAfn[]' <<< "$BNODE_REPORT"`
+BNODE_PLAIN=`jq -r '.[].s.node[].id' <<< "$BNODE_REPORT"`
+
+
+# Prepend '_:' to the bnode id
+BNODE_HPAFN=${BNODE_ID_HPAFN:+_:$BNODE_ID_HPAFN}
+
+BNODE=${BNODE_HPAFN:-${BNODE_PLAIN:-foo}}
+
+export BNODE
+
+#echo "GOT $BNODE_ID_HPAFN --- $BNODE_HPAFN ---  $BNODE"
+
+
+#BNODE_ID=`sparql-integrate select-bnode.sparql --jq | jq -r '.[].s.id'`
+
+if [ -z "$BNODE" ]; then error=1; echoerr "[ERROR] received an empty; expected a blank node id from the given service. Is data loaded?"; fi
+[ $error -eq 0 ] || exit $error
+
+
+echoerr "[INFO] Retrieved blank node id: $BNODE"
+
+
+sparql-integrate test-bnode-lookups.sparql spo.sparql
+
+
 
 
 
---
+++ test.sh
@@ -0,0 +1,35 @@
+# GraphDB
+./bnode-analyze.sh -n graphdb-free -v 8.9.0 http://localhost:7200/repositories/test
+
+
+# Virtuoso
+./bnode-analyze.sh http://localhost:8890/sparql
+
+
+# Blazegraph
+./bnode-analyze.sh http://localhost:9999/blazegraph/
+
+
+# Stardog
+./bnode-analyze.sh -n stardog -v 6.1.3 http://anonymous:anonymous@localhost:5820/qrowd/query
+
+# Jena / In-Memory
+./bnode-analyze.sh -n jena-fuseki-mem -v 3.10.0 http://localhost:3030/mem/
+
+
+# Jena / TDB
+./bnode-analyze.sh -n jena-fuseki-tdb2 -v 3.10.0 http://localhost:3030/tdb2/
+
+
+
+# RDF4J - In-Memory
+
+
+
+# RDF4J - 
+
+
+# rdflib
+
+
+
