c355ea2a0ed99b6df3b9341211060417f3779801
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-07-23 19:08:57 +0200

    Updated server component

--- bnode-proxy-parent/bnode-proxy-cli/src/main/java/org/aksw/bnode_proxy/cli/main/ControllerSparqlRewrite.java
+++ bnode-proxy-parent/bnode-proxy-cli/src/main/java/org/aksw/bnode_proxy/cli/main/ControllerSparqlRewrite.java
@@ -2,15 +2,21 @@ package org.aksw.bnode_proxy.cli.main;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import org.aksw.jena_sparql_api.algebra.expr.transform.ExprTransformVirtualBnodeUris;
 import org.aksw.jena_sparql_api.core.utils.RDFDataMgrEx;
+import org.aksw.jena_sparql_api.core.utils.ReactiveSparqlUtils;
 import org.aksw.jena_sparql_api.user_defined_function.UserDefinedFunctions;
 import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.sparql.function.user.UserDefinedFunctionDefinition;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -20,6 +26,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerSparqlRewrite {
+
+	@GetMapping("/profiles")
+	@CrossOrigin(origins = "*")
+	public List<String> profiles() {
+		Model model = RDFDataMgr.loadModel("bnode-rewrites.ttl");
+		RDFDataMgrEx.execSparql(model, "udf-inferences.sparql");
+
+		List<String> result = ReactiveSparqlUtils.execSelectQs(() -> QueryExecutionFactory.create("SELECT DISTINCT ?p { ?s <http://ns.aksw.org/jena/udf/profile> ?p }", model))
+			.map(b -> b.get("p"))
+			.filter(Objects::nonNull)
+			.filter(RDFNode::isURIResource)
+			.map(RDFNode::asResource)
+			.map(Resource::getURI)
+			.toList()
+			.blockingGet();
+		
+		return result;
+	}
+
+	
 	@GetMapping("/rewrite")
 	@CrossOrigin(origins = "*")
 	public String rewrite(@RequestParam String profile, @RequestParam String query) {
